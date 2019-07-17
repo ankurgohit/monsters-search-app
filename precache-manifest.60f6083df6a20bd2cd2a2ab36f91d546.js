a35f828bdd48c1005d4e6ecb748e3106af82d6cc
ankurgohit_N0287 <ankur.gohit@jabong.com>
2019-07-17 13:13:10 +0530

    Updates

---
+++ precache-manifest.60f6083df6a20bd2cd2a2ab36f91d546.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "036ad354f30aae79390ec4195f4e1e1c",
+    "url": "/monsters-search-app/index.html"
+  },
+  {
+    "revision": "bb6d4bbf3bc07cd1eaa3",
+    "url": "/monsters-search-app/static/css/main.36026095.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-search-app/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "bb6d4bbf3bc07cd1eaa3",
+    "url": "/monsters-search-app/static/js/main.50752206.chunk.js"
+  },
+  {
+    "revision": "2da9883d53c959de1ced",
+    "url": "/monsters-search-app/static/js/runtime~main.8b3c6357.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-search-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
